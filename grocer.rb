d37ad3e9b380f9b74e84c3da3a0db65dd661c510
Henry Zhang <henryyihengzhang@gmail.com>
2018-03-04 22:24:10 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ def apply_clearance(cart)
   cart.each do |item|
     attributes = item[1]
     if attributes[:clearance] == true 
-      discount = attributes[:price] * (.80)
+      discount = attributes[:price] * 0.80
 
 end
 
