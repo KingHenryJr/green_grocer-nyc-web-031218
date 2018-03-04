2d2cf9f8c98d9989031bef3a05d202c9091f00d8
Henry Zhang <henryyihengzhang@gmail.com>
2018-03-04 22:08:19 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,42 @@
+require 'pry'
+
+
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  veggies = []
+  uniq_veggies = []
+  cart.each do |data_hash|
+      veggies << data_hash.keys
+  end
+  veggies = veggies.flatten
+  uniq_veggies = veggies.uniq 
+  uniq_veggies.each do |name|
+    
+    insides = cart.find{ |i| i.keys[0] == name}
+    
+    new_cart[name] = insides.values[0] 
+    
+    new_cart[name][:count] = 0
+    
+    end  
+    
+    veggies.each do |veggie|
+      new_cart[veggie][:count] += 1 
+    end
+  
+  return new_cart  
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
+  coupons.each do |info|
+    coupon_name = info[:item] + " W/COUPON"  
+    binding.pry
+    cart[coupon_name][:price] = info[:cost]
+    cart[coupon_name][:clearance] = cart[info[:item]][:clearence]
+    cart[info[:item]][:count] -= info[:num]
+  end
+  return cart
 end
 
 def apply_clearance(cart)
